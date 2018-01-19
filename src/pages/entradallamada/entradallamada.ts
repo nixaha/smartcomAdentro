0c0f6193da07a5cc3e5b5a18fc8631b773f15633
Nixaha <nixaha30saucedo@gmail.com>
2018-01-19 14:13:10 -0600

    toast y modificacion y diseño

--- src/pages/entradallamada/entradallamada.ts
+++ src/pages/entradallamada/entradallamada.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-entradallamada',
@@ -7,9 +8,24 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class EntradallamadaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public toastCtrl: ToastController) {
+  }
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'El portón se esta Abriendo',
+      duration: 3000
+    });
+    toast.present();
   }
 
+  Toast() {
+    let toast = this.toastCtrl.create({
+      message: 'El portón se esta Cerrando',
+      duration: 3000
+    });
+    toast.present();
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad EntradallamadaPage');
   }
